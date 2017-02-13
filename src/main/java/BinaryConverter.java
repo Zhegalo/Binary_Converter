7f99e714560d87a5f195168816283ed40e942ccc
Hanna Zhahala <Hanna_Zhahala@epam.com>
2017-02-13 20:14:20 +0100

    BinaryConverter implementation

---
+++ src/main/java/BinaryConverter.java
@@ -0,0 +1,21 @@
+/**
+ * Created by Hanna_Zhahala on 13.02.2017.
+ */
+public class BinaryConverter {
+
+    /*public static void main(String[] args) {
+        int i = 4;
+        System.out.println("Number is " + i);
+        System.out.println("Binary is " + Integer.toBinaryString(i));
+    }*/
+
+
+    public static void main(String[] args) {
+        String i = new BinaryConverter().binaryConverter(3);
+        System.out.println(i);
+    }
+
+    public String binaryConverter(int number) {
+        return ("Binary is " + Integer.toBinaryString(number));
+    }
+}
